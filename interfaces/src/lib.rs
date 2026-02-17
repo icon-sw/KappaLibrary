a1cc8d7ac48533efc0cd3eddd5bba5a499c857c4
Sofia Silvestri <sofia.silvestri@glaukonis.com>
2026-02-17 17:44:52 +0100

    Interfaces

---
+++ interfaces/src/lib.rs
@@ -0,0 +1,4 @@
+pub mod tcp_ip_client;
+pub mod tcp_ip_server;
+pub mod udp_ip_client;
+pub mod udp_ip_server;
---
+++ interfaces/src/tcp_ip_client.rs
@@ -0,0 +1,33 @@
+use std::{collections::HashMap, fmt, sync::{Arc, Mutex, MutexGuard}};
+use memory_macro::K2Memory;
+use processor_macro::K2ProcessorBlock;
+
+use k2_stream::{memory::{DataHeader, MemoryTrait}, processors::{ProcessorBlockTrait, ProcessorHeader, ProcessorTrait, StreamBlock, StreamState}};
+
+#[derive(K2Memory, K2ProcessorBlock)]
+pub struct TcpIpClient {
+    pub name: String,
+    pub header: ProcessorHeader,
+    stream_block: StreamBlock,
+    state: Arc<Mutex<StreamState>>,
+}
+impl TcpIpClient {
+    pub fn new(name: String) -> Result<Self, ()> {
+        let mut ret = Self {
+            name: name.clone(),
+            header: ProcessorHeader {
+                proc_name: "".to_string(),
+                description: "".to_string(),
+                version: "0.1.0".to_string(),
+                author: "".to_string(),
+                email: "".to_string(),
+                license: "".to_string(),
+                repository: "".to_string(),
+            },
+            stream_block: StreamBlock::new(),
+            state: Arc::new(Mutex::new(StreamState::Initialized)),
+        }
+        Ok(ret)
+    }
+}
+        
